de017b22a2457286cc04a99191e66f55c4c3059c
Andrew Pecha <andrew.pecha@gmail.com>
2024-03-18 21:57:24 -0500

    dashing not working at all :D

--- DashAttack/player_dash_attack.gd
+++ DashAttack/player_dash_attack.gd
@@ -2,13 +2,19 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+const DASH_SPEED = 2000.0
+var is_dashing: bool = false
+var dash_target: Vector2
 
 
 func _physics_process(delta: float) -> void:
-	handle_movement()
+	if !is_dashing:
+		handle_movement()
+	
+	if is_dashing && global_position >= dash_target:
+		is_dashing = false
+
+	move_and_slide()
 
 func handle_movement() -> void:
 	var direction_x := Input.get_axis("left", "right")
@@ -26,4 +32,16 @@ func handle_movement() -> void:
 
 	velocity = velocity.normalized() * SPEED
 
-	move_and_slide()
+	
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("rmb") && !is_dashing:
+		dash()
+
+func dash() -> void:
+	is_dashing = true
+	dash_target = get_global_mouse_position()
+	velocity = (dash_target - global_position).normalized() * DASH_SPEED
+	if velocity.length() > (dash_target - global_position).length():
+		velocity = dash_target - global_position
+	
