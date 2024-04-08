b7e791ae6bdd983a3c77d430c712e25fab6efc80
Andrew Pecha <andrew.pecha@gmail.com>
2024-04-07 21:58:45 -0500

    spawner rotates and has variable rotation speed

--- BulletHellper/BulletHellperObjectives.md
+++ BulletHellper/BulletHellperObjectives.md
@@ -1,7 +1,10 @@
 # Bullet Hellper
 
 1. ~~Spawner that can shoot projectile every second~~
-2. Spawner has varibale timing
-3. Spawner can rotate at fixed speed
-4. Spawner rotates at variable speed
+2. ~~Spawner has variable timing~~
+3. ~~Spawner can rotate at fixed speed~~
+4. ~~Spawner rotates at variable speed~~
 5. Spawner has an option to spawn under partner or world
+6. Spawner lives under a parent
+7. Parent can create spawners evenly spread out
+8. Spawners are rotated based on initial position
--- BulletHellper/bh_spawner.gd
+++ BulletHellper/bh_spawner.gd
@@ -1,25 +1,39 @@
 extends Node2D
 
 @export var projectile: PackedScene
-var shoot_cooldown_timeout: float = 1.0
+
+@export var rotate_self: bool = true
+@export var shoot_cooldown_timeout: float = 1.0
 var shoot_cooldown_counter: float = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@export var full_rotation_time: float = 5.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	handle_rotation(delta)
+	handle_shooting(delta)
+	
+func handle_rotation(delta: float):
+	if rotate_self:
+		rotate(2 * PI * (delta / full_rotation_time))
+
+func handle_shooting(delta: float) -> void:
 	shoot_cooldown_counter += delta
 	if shoot_cooldown_counter >= shoot_cooldown_timeout:
 		shoot()
 		shoot_cooldown_counter = 0
 		
 func shoot() -> void:
-	var shot_direction = Vector2(1, 0)
+	var shot_direction: Vector2
+	
+	if rotate_self:
+		shot_direction = Vector2.RIGHT.rotated(rotation)
+	else:
+		shot_direction = Vector2.RIGHT
+	
 	var projectile_instance = projectile.instantiate()
 	projectile_instance.direction = shot_direction
-	add_child(projectile_instance)
+	var world = get_tree().root.get_node("World")
+	world.add_child(projectile_instance)
 	
 	#print("shooting!")
