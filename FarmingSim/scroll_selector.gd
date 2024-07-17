2fa66cdf19e07b7575e698d707fce1f35d41e62b
Andrew Pecha <andrew.pecha@gmail.com>
2024-07-16 20:32:33 -0500

    skills can scroll both directions

--- FarmingSim/scroll_selector.gd
+++ FarmingSim/scroll_selector.gd
@@ -29,3 +29,18 @@ func _input(event: InputEvent) -> void:
 				next_skill.get_node('ColorRect').visible = true
 				player.set_skill(next_skill)
 				return
+				
+	if event.is_action_released('scroll_up'):
+		var texture_rects = $Menu.get_children()
+		for x in len(texture_rects):
+			var rect = texture_rects[x]
+			#rect.get_node('ColorRect').visible = !rect.get_node('ColorRect').visible
+			if rect.get_node('ColorRect').visible:
+				var next_skill_index = x - 1
+				if next_skill_index == -1:
+					next_skill_index = len(texture_rects) - 1
+				var next_skill = texture_rects[next_skill_index]
+				rect.get_node('ColorRect').visible = false
+				next_skill.get_node('ColorRect').visible = true
+				player.set_skill(next_skill)
+				return
