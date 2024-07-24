734e781ce9aaa060dc4a3f60a0450aa8dd9bf4cc
Andrew Pecha <andrew.pecha@gmail.com>
2024-07-23 22:12:23 -0500

    basic bezier projectile functioning

--- IsometricTapRpg/healer.gd
+++ IsometricTapRpg/healer.gd
@@ -36,15 +36,15 @@ func _input(event: InputEvent) -> void:
 		
 func shoot_at(target: Vector3) -> void:
 	var shot_vector = Vector3(target.x, position.y, target.z) - position
-	shot_vector = shot_vector.rotated(Vector3(0, 1, 0), deg_to_rad(90))
-	var shot_vector_2 = shot_vector.rotated(Vector3(0, 1, 0), deg_to_rad(-180))
+	#shot_vector = shot_vector.rotated(Vector3(0, 1, 0), deg_to_rad(90))
+	#var shot_vector_2 = shot_vector.rotated(Vector3(0, 1, 0), deg_to_rad(-180))
 
 	var projectile_instance: PlaceholderProjectile = projectile.instantiate()
 	projectile_instance.global_position = global_position
-	projectile_instance.shot_direction = shot_vector.normalized()
+	projectile_instance.set_destination(target)
 	get_tree().root.get_node("World").add_child(projectile_instance)
 	
-	var projectile_instance_2: PlaceholderProjectile = projectile.instantiate()
-	projectile_instance_2.global_position = global_position
-	projectile_instance_2.shot_direction = shot_vector_2.normalized()
-	get_tree().root.get_node("World").add_child(projectile_instance_2)
+	#var projectile_instance_2: PlaceholderProjectile = projectile.instantiate()
+	#projectile_instance_2.global_position = global_position
+	#projectile_instance_2.shot_direction = shot_vector_2.normalized()
+	#get_tree().root.get_node("World").add_child(projectile_instance_2)
--- IsometricTapRpg/placeholder_projectile.gd
+++ IsometricTapRpg/placeholder_projectile.gd
@@ -4,6 +4,39 @@ class_name PlaceholderProjectile
 var shot_direction: Vector3
 const SPEED = 8
 
+var bezier_speed: float = 2.0
+var deviation_distance: float = 15
+var deviation_angle: float = 90
+
+var p0: Vector3
+var p1: Vector3
+var p2: Vector3
+
+var t: float = 0.0
+
+func _ready():
+	set_physics_process(false)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	position += shot_direction * SPEED * delta
+	#position += shot_direction * SPEED * delta
+	
+	#if t >= 1.0:
+		#queue_free()
+		#return
+	
+	if t < 1.0:
+		t += bezier_speed * delta
+		
+	position = position.bezier_interpolate(p0, p1, p2, t)
+
+# lots of code modified from https://www.youtube.com/watch?v=4SHqHFWYg-s&t=106s
+func set_destination(destination):
+	p0 = global_position
+	p2 = destination
+	
+	var tilted_unit_vector = (p2-p0).normalized().rotated(Vector3(0, 1, 0), deg_to_rad(-deviation_angle))
+	print("tilted vector: " + str(tilted_unit_vector))
+	p1 = p0 + deviation_distance * tilted_unit_vector
+	
+	call_deferred("set_physics_process", true)
