d398e0a3dd581d38d7aeb826d9cd6887c49058c4
Andrew Pecha <andrew.pecha@gmail.com>
2024-07-27 12:43:53 -0500

    successfully rotating around the player

--- BezierPractice/player.gd
+++ BezierPractice/player.gd
@@ -2,15 +2,18 @@ extends StaticBody2D
 
 @export var marker: PackedScene
 @export var speed: float = 5.0
+var marker_instance: Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var marker_instance = marker.instantiate()
+	marker_instance = marker.instantiate()
 	marker_instance.global_position = Vector2(global_position.x, global_position.y - 200)
 	get_tree().root.get_node("World").add_child.call_deferred(marker_instance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var direction_to_marker:Vector2 = (marker_instance.global_position - global_position)
+	var new_marker_position = direction_to_marker.normalized().rotated(deg_to_rad(20 * delta)) * direction_to_marker.length()
+	marker_instance.global_position = global_position + new_marker_position
