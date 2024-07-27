16a46f0abad349144bbe60e5b303bec63deb939e
Andrew Pecha <andrew.pecha@gmail.com>
2024-07-27 12:36:13 -0500

    bezier practice base marker setup

--- BezierPractice/player.gd
+++ BezierPractice/player.gd
@@ -1,10 +1,14 @@
 extends StaticBody2D
 
 @export var marker: PackedScene
+@export var speed: float = 5.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var marker_instance = marker.instantiate()
+	marker_instance.global_position = Vector2(global_position.x, global_position.y - 200)
+	get_tree().root.get_node("World").add_child.call_deferred(marker_instance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
